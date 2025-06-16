227612e5f62b290fd38e1fc8dd2d63049e09f437
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-16 17:05:17 +0530

    Initial commit: spring-boot-with-swagger project created

---
+++ spring-boot-with-swagger/src/main/java/com/expertise/swagger/example/spring_boot_with_swagger/SpringBootWithSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.expertise.swagger.example.spring_boot_with_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSwaggerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-with-swagger/src/test/java/com/expertise/swagger/example/spring_boot_with_swagger/SpringBootWithSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertise.swagger.example.spring_boot_with_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
