7ef74b3154892adc1a43964f764e0d1c57e8474d
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-18 11:53:53 +0530

    Initial commit: spring-boot-junit-mockito example added

---
+++ spring-boot-junit-mockito/src/test/java/com/expertise/book/testing/example/SpringBootJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertise.book.testing.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
