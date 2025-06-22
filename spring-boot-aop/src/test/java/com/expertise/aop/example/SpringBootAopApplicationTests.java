4822df4d9f03d66a00f59391eeb11d3e4b8873a7
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-22 21:06:00 +0530

    Initial commit: spring-boot-aop example created for Aspect-Oriented Programming concept

---
+++ spring-boot-aop/src/test/java/com/expertise/aop/example/SpringBootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertise.aop.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
